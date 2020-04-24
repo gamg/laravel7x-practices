7da6a8330cd5b331a972d67f2d132cfa3acd4a83
gamg <adolfz10@gmail.com>
2020-04-24 17:32:54 -0400

    examples with fluent string

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,14 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+use Illuminate\Support\Str;
+
+Route::get('/string', function () {
+    $texto = ' super duper framework Laravel 6.x super';
+    $result = Str::of($texto)->trim()
+        ->beforeLast('super')
+        ->replace('6.x', '7.x')
+        ->after('super');
+    dd($result);
+});
